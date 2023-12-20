1f4c1308c8bbc17beb85921196a1338eee3ed9bc
ozgursefer <seferoglu19@itu.edu.tr>
2023-12-20 20:03:51 +0300

    Error Pages Added

--- main.py
+++ main.py
@@ -18,75 +18,86 @@ app = Flask(__name__)
 # the associated function.
 
 
-@app.route("/") #redirects user to search flights page as default page
+@app.route("/")  # redirects user to search flights page as default page
 def index():
-    return redirect('/search_flight')
+    return redirect("/search_flight")
+
 
 @app.route("/search_flight")
 def search_flight():
     cities = Bflights.get_cities()
 
-    return render_template("search_flight.html", optionsFrom=cities, optionsWhere=cities, optionsDates=[])
+    return render_template(
+        "search_flight.html", optionsFrom=cities, optionsWhere=cities, optionsDates=[]
+    )
 
-@app.route("/searched_flights", methods=['POST'])
+
+@app.route("/searched_flights", methods=["POST"])
 def searched_flights():
-    selected_From = request.form['dropdownFrom']
-    selected_Where = request.form['dropdownWhere']
+    selected_From = request.form["dropdownFrom"]
+    selected_Where = request.form["dropdownWhere"]
 
     searched_flights = Bflights.search_flights(selected_From, selected_Where, 10)
 
     return render_template("searched_flights.html", flights=searched_flights)
 
-@app.route("/buy_ticket", methods=['POST'])
+
+@app.route("/buy_ticket", methods=["POST"])
 def buy_ticket():
-    selected_flight_id = request.form['flight_id']
+    selected_flight_id = request.form["flight_id"]
     return render_template("buy_ticket.html", flight_id=selected_flight_id)
 
-@app.route("/show_ticket", methods=['POST'])
+
+@app.route("/show_ticket", methods=["POST"])
 def show_ticket():
-    
     return render_template("show_ticket.html")
 
-@app.route("/show_ticket_error", methods=['POST'])
+
+@app.route("/show_ticket_error", methods=["GET"])
 def show_ticket_error():
-    
     return render_template("show_ticket_error.html")
 
+
 ##############################################################################################
+
 
 @app.route("/check_in")
 def check_in():
     return render_template("check_in.html")
 
-@app.route("/boarding_pass") 
+
+@app.route("/boarding_pass")
 def boarding_pass():
     selected_seats = Bseats.Search()
-    return render_template("boarding_pass.html", seats = selected_seats)
+    return render_template("boarding_pass.html", seats=selected_seats)
 
-@app.route("/show_boarding_pass") 
+
+@app.route("/show_boarding_pass")
 def show_boarding_pass():
-
     return render_template("show_boarding_pass.html")
 
+
 @app.route("/show_boarding_pass_error")
-def show_boarding_pass_error(): 
-    
+def show_boarding_pass_error():
     return render_template("show_boarding_pass_error.html")
 
+
 ###############################################################################################
+
 
 @app.route("/flight_status")
 def flight_status():
     return render_template("flight_status.html")
 
-@app.route("/flight_cancel") 
+
+@app.route("/flight_cancel")
 def flight_cancel():
     return render_template("flight_cancel.html")
 
-@app.route("/flight_cancel_error") 
+
+@app.route("/flight_cancel_error")
 def flight_cancel_error():
     return render_template("flight_cancel_error.html")
-
 
 
 # main driver function
